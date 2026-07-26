e429785fec91e4d169d22cbdc3257d7457b1282d
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-07-26 16:06:54 -0500

    Added the responsebody as well

--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
+# For Request body
 
 class Message(BaseModel):
     role: str
@@ -13,5 +14,25 @@ class Request(BaseModel):
     max_tokens: Optional[int] = None
     n: Optional[int] = None
 
-##
+# For Response Body
+class ResponseMessage(BaseModel):
+    role: str
+    content: str
 
+class Choice(BaseModel):
+    index: int
+    message: ResponseMessage
+    finish_reason: str
+
+class Usage(BaseModel):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+
+class Response(BaseModel):
+    id: str
+    object: str
+    created: int
+    model: str
+    choices: list[Choice]
+    usage: Usage
