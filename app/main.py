84e15176e92ea77492471aa99bbb9e8c82ea631f
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-07-28 19:20:28 -0500

    Added post method

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-#
+from fastapi import FastAPI
+from app.schemas.chat import Request, Response
+
+app = FastAPI()
+
+@app.post("/v1/chat/completions")
+def chat_completions(request: Request):
+    return {"message": "not implemented yet"}
