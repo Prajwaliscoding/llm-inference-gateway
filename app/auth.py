30d7f4a598ca71585886f1cc2f2e60fe504cc74f
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-07-30 19:14:59 -0500

    Added auth.py for checking the clients request is valid or not

---
+++ app/auth.py
@@ -0,0 +1,9 @@
+from app.config import settings
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+security = HTTPBearer()
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if credentials.credentials != settings.gateway_api_token:
+        raise HTTPException(status_code=401, detail="Invalid or missing token")
