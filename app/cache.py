2a998cbfc59bfa9dd776953659801491af6b3242
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-08-05 14:29:27 -0500

    Added find_cache_in_redis to lookup if we have the request already

--- app/cache.py
+++ app/cache.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 
+from app.redis_client import redis_client
 from app.schemas.chat import Request
 
 def build_cache_key(request: Request) -> str:
@@ -14,6 +15,12 @@ def build_cache_key(request: Request) -> str:
     }
 
     serialized = json.dumps(payload, sort_keys=True)
-    
+
     return "cache:" + hashlib.sha256(serialized.encode()).hexdigest()
 
+async def find_cache_in_redis(cache_key: str):
+    cache = await redis_client.get(cache_key)
+
+    if cache is None:
+        return None
+    return json.loads(cache)
