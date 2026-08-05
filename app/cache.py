bb5f920e8d5a4fdf655f4e1d5717af421a20e753
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-08-05 14:50:57 -0500

    Added save and get cache

--- app/cache.py
+++ app/cache.py
@@ -1,8 +1,12 @@
+# Cache key and Cache value for key-value pair in Redis
+# cache_key = hashed key to check for every request
+# cache_value = the response made by our provider(OpenAI or Claude)
+
 import hashlib
 import json
 
 from app.redis_client import redis_client
-from app.schemas.chat import Request
+from app.schemas.chat import Request, Response
 
 def build_cache_key(request: Request) -> str:
     messages_list = []
@@ -18,9 +22,15 @@ def build_cache_key(request: Request) -> str:
 
     return "cache:" + hashlib.sha256(serialized.encode()).hexdigest()
 
-async def find_cache_in_redis(cache_key: str):
+
+async def save_cache_value(cache_key:str, cache_value:Response, ttl:int=3600):
+    serialized = cache_value.model_dump_json()
+    await redis_client.set(cache_key, serialized, ex=ttl)
+
+async def find_cache_key(cache_key: str):
     cache = await redis_client.get(cache_key)
 
     if cache is None:
         return None
     return json.loads(cache)
+
